3db3c9331520891094a28844069c5c01e267bee3
Jesús Salas <jesusalas18@gmail.com>
2018-04-21 18:59:46 -0600

    Se cambio diseño buscador

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,10 +1,28 @@
 <template>
    
    <div class="container">
+      <nav class="navbar navbar-dark bg-primary col-sm-6">
+         <a class="navbar-brand" href="#">
+            <form class="form-inline">
+               <i class="material-icons">search</i>
+               <input type="text" placeholder="Buscar" class="form-control btn-primary" v-model="employee">
+               <i class="material-icons">account_circle</i>
+            </form>
+         </a>
+      </nav>
+      <nav class="navbar navbar-dark bg-dark col-sm-6">
+         <a class="navbar-brand" href="#">
+            <form class="form-inline">
+               <i class="material-icons">arrow_back</i>
+               <input type="text" placeholder="Buscar" class="form-control btn-none-color" v-model="employee">
+               <i class="material-icons">close</i>
+            </form>
+         </a>
+      </nav>
       <div class="row">
-         <div class="col-sm-8">
+         <div class="col-sm-6">
             <ul class="list-group">
-               <li class="list-group-nav">
+               <li class="list-none-line">
                   <nav class="navbar navbar-dark bg-primary">
                      <form class="form-inline">
                         <a class="navbar-brand" href="#">Empleado</a>
@@ -20,7 +38,14 @@
                      </form>
                   </nav>
                </li>
-               <li v-for="task in searchEmployee" class="list-group-item"> {{task.empleado}}</li>
+            </ul>
+            <ul class="list-group list-container">
+               <li v-for="task in searchEmployee" class="list-group-item"> 
+               <ul>
+                  <li class="list-none-line">{{ task.empleado }}</li>
+                  <li class="list-none-line">{{ task.usuario}} - {{ task.contrasenia }}</li>
+               </ul>
+               </li>
             </ul>
          </div>
       </div>
@@ -59,7 +84,24 @@
 
 </script>
 <style>
-   .list-group-nav{
+   .list-none-line{
       list-style: none;
+   }
+   .list-container{
+      max-height: 740px;
+      overflow-y:scroll; 
+   }
+   .btn-none-color{
+      background-color: transparent;
+      border-color: transparent;
+      color: white;
+   }
+   .btn-none-color:hover, .btn-none-color:focus, .btn-none-color::placeholder{
+      background-color: transparent;
+      border-color: transparent;
+      color: white;
+   }
+   .btn-none-color::placeholder{
+      color: #848484;
    }
 </style> 
